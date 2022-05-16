3a04884a33adf79b98ef2a7cff10ea0c20ebd5b1
Elyzza Jane Abrigo <elyzza.abrigo@gmail.com>
2022-05-16 22:23:21 +0800

    [MOD] remove duplicate strings

--- Lexical/src/lexer/LexerAnalyzer.java
+++ Lexical/src/lexer/LexerAnalyzer.java
@@ -79,15 +79,6 @@ class Position {
 //# TOKENS
 //#######################################
  class Token {
-    private final String TT_INT = "INT";
-    private final String TT_FLOAT = "FLOAT";
-    private final String TT_PLUS = "PLUS";
-    private final String TT_MUL = "MINUS";
-    private final String TT_DIV = "DIV";
-    private final String TT_LPAREN  = "LPAREN";
-    private final String TT_RPAREN  = "RPAREN";
-    
-    
     private String type;
     private String value;
     
